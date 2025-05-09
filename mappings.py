ef774026fbd82ca73b627db1a985a4b91b65a538
Logan <logan.sritharan1912@gmail.com>
2025-05-09 17:21:12 +0100

    mappings for common mappings used to remove repetition in MonopolyDeal.m. test.py for testing environment

---
+++ mappings.py
@@ -0,0 +1,47 @@
+# Number of cards required for a set
+set_length = {
+    "Dark Blue": 2,
+    "Brown": 2,
+    "Light Green": 2,
+    "Green": 3,
+    "Light Blue": 3,
+    "Red": 3,
+    "Yellow": 3,
+    "Orange": 3,
+    "Pink": 3,
+    "Black": 4
+}
+
+# Colour → Number
+colour_mapping = {
+    "Dark Blue": 0,
+    "Brown": 1,
+    "Light Green": 2,
+    "Green": 3,
+    "Light Blue": 4,
+    "Red": 5,
+    "Yellow": 6,
+    "Orange": 7,
+    "Pink": 8,
+    "Black": 9
+}
+
+action_description = {
+    0: "Skip",
+    1: "Move Property",
+    2: "Play Money",
+    3: "Play Property",
+    4: "Play Wild Property",
+    5: "Sly Deal",
+    6: "Forced Deal",
+    7: "Debt Collector",
+    8: "It's My Birthday",
+    9: "Deal Breaker",
+    10: "Red/Yellow Rent",
+    11: "Green/DarkBlue Rent",
+    12: "Pink/Orange Rent",
+    13: "Blue/LightGreen Rent",
+    14: "Blue/LightBlue Rent",
+    15: "Wild Rent",
+    16: "Just Say No"
+}
