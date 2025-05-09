3b4c2eae65580c3b33f05961e2810c1d98180aa7
Logan Sritharan <logan.sritharan@gmail.com>
2025-05-09 17:21:12 +0100

    mappings for common mappings used to remove repetition in MonopolyDeal.m. test.py for testing environment

---
+++ test.py
@@ -0,0 +1,23 @@
+from MonopolyDeal import MonopolyDeal
+
+env = MonopolyDeal(render_mode="human")
+env.reset(seed=42)
+
+for agent in env.agent_iter():
+    observation, reward, termination, truncation, info = env.last()
+
+    if termination or truncation:
+        action = None
+    else:
+        # invalid action masking is optional and environment-dependent
+        if "action_mask" in info:
+            mask = info["action_mask"]
+        elif isinstance(observation, dict) and "action_mask" in observation:
+            mask = observation["action_mask"]
+        else:
+            mask = None
+        action = env.action_space(agent).sample(mask) # this is where you would insert your policy
+
+    env.step(action)
+
+env.close()
